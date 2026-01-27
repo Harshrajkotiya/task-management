8617ceb4e0a6f6911297c9152e258eb7fdeba605
harsh <harshrajkotiya2002@gmail.com>
2026-01-28 02:10:31 +0530

    Add CORS options

--- server/src/index.js
+++ server/src/index.js
@@ -6,7 +6,14 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN || '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
